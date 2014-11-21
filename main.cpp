b2ad8ba1abe15c73fb2ce8495db1847cc046eaf5
shrekshao <shrekshao@gmail.com>
2014-11-21 10:34:17 -0800

    multi objs, fix adaptive

--- main.cpp
+++ main.cpp
@@ -23,8 +23,11 @@
 
 
 #include <iostream>
+#include <fstream>
 #include <stdlib.h>
 #include <cstring>
+#include <string>
+#include <vector>
 #include "PolyVector.h"
 #include "BezReader.h"
 
@@ -51,34 +54,60 @@
 #endif
 
 using namespace std;
+
 float zoom = 1;
-float x_angle = 0;
-float y_angle = 0;
-float x_trans = 0;
-float y_trans = 0;
-int lastx=0;
-int lasty=0;
 int window_w = 600;
 int window_h = 600;
-
 float camera_dist = 10;
 Eigen::Vector3f pos_camera(0,0,-10);
+//for mouse
+int lastx=0;
+int lasty=0;
 
+int selected_obj_id = -1;
+bool f_multiple_obj = false;
+
+struct ObjTransformInfo
+{
+	Eigen::Vector3f center;
+	float x_angle;
+	float y_angle;
+	float x_trans;
+	float y_trans;
+
+	ObjTransformInfo()
+		:x_angle(0),y_angle(0)
+		,x_trans(0),y_trans(0)
+	{
+	}
+};
+
+
+int num_objs = 1;
+vector<ObjTransformInfo> vec_objTransformInfo;
+vector<PolyVector> vec_polyVector;
+
+Eigen::Vector3f center;
 
 bool _hiddenLine = false;
 bool _flatShading = false;
 
 unsigned char Buttons[3] = {0};
 
-PolyVector teapot;
-//Eigen::Vector3f point;
-//Eigen::Vector3f normal;
-Eigen::Vector3f center;
+//PolyVector teapot;
+//Eigen::Vector3f center;
 
 void handleKeypress(unsigned char key, int x, int y) {
 	switch (key) {
 		case 27: //Escape key
 			exit(0);
+			break;
+		case 'z':
+			//change selected obj
+			if(f_multiple_obj)
+			{
+				selected_obj_id = (selected_obj_id+1) % (num_objs+1);
+			}
 			break;
 		case 's':
 			_flatShading = !_flatShading;
@@ -96,7 +125,7 @@ void handleKeypress(unsigned char key, int x, int y) {
 		case '-':
 		case '_':
 			{
-				zoom = zoom - .1;
+				zoom = zoom + .1;
 				//Eigen::Vector3f vector_zoom((center - pos_camera)*zoom);
 				//pos_camera += vector_zoom;
 				//cout<<pos_camera<<endl;
@@ -106,13 +135,15 @@ void handleKeypress(unsigned char key, int x, int y) {
 		case '=':
 		case '+':
 			{
-				zoom = zoom + .1;
+				zoom = zoom - .1;
 				//Eigen::Vector3f vector_zoom((center - pos_camera)*zoom);
 				//pos_camera += vector_zoom;
 				//cout<<pos_camera<<endl;
 				
 				break;
 			}
+		default:
+			break;
 	}
 }
 
@@ -123,8 +154,8 @@ Eigen::Vector3f getCenter(PolyVector& shape)
 	Eigen::Vector3f max;
 	int poly_size = shape.getPolySize();
 	for (int i = 0; i < poly_size; i += 1){		
-		for(int j = 0; j < teapot.getVertexSize(i); j+= 1){
-			vertex = teapot.getVertex(i, j).p;
+		for(int j = 0; j < shape.getVertexSize(i); j+= 1){
+			vertex = shape.getVertex(i, j).p;
 			if(i == 0 && j == 0){
 				min = vertex;
 				max = vertex;
@@ -205,78 +236,132 @@ void drawScene() {
 	glutPostRedisplay();
 	
 
+	//gluLookAt(pos_camera.x(),pos_camera.y(),pos_camera.z()
+	//	,center.x(),center.y(),center.z(),0,-1,0);
 	gluLookAt(pos_camera.x(),pos_camera.y(),pos_camera.z()
-		,center.x(),center.y(),center.z(),0,-1,0);
-
+		,0,0,0,0,-1,0);
 	
 	//light used to be here
 
 
 	
 
-	
-	glMatrixMode(GL_MODELVIEW);
-	glPushMatrix();
-	
-
-	glLoadIdentity();
-	
-	//glTranslatef(0, 0, zoom);
-	glTranslatef(x_trans, y_trans, 0);
-	glRotatef(x_angle, 1, 0, 0);
-	glRotatef(y_angle, 0, 1, 0);
-	glTranslatef(-center.x(),-center.y(),-center.z());
-	//glTranslatef(-center.x(), -center.y(), -center.z());
-	
-
-	
-	//Determine the specularity, emissivity, and shininess parameters, based on
-	//variables that can be toggled with keystrokes
-	
-	float specularity = 1;
-	
-	float emissivity = 0;
-	
-	float shininess = 12;
-
-	
-	//The color of the sphere
-	GLfloat materialColor[] = {0.2f, 0.2f, 1.0f, 1.0f};
-	//The specular (shiny) component of the material
-	GLfloat materialSpecular[] = {specularity, specularity, specularity, 1.0f};
-	//The color emitted by the material
-	GLfloat materialEmission[] = {emissivity, emissivity, emissivity, 1.0f};
-	
-	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, materialColor);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, materialSpecular);
-	glMaterialfv(GL_FRONT, GL_EMISSION, materialEmission);
-	glMaterialf(GL_FRONT, GL_SHININESS, shininess); //The shininess parameter
-	
 
 
-	Eigen::Vector3f point;
-	Eigen::Vector3f normal;
 	
-	int poly_size = teapot.getPolySize();
-	for (int i = 0; i < poly_size; i += 1){
-		//cout << "Polygon " << i << ":" << endl;
-		if(_hiddenLine){
-			glBegin(GL_LINE_LOOP);
-		} else {
-			glBegin(GL_POLYGON);
+
+	for ( int i = 0; i<num_objs; i++)
+	{
+
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+		glLoadIdentity();
+
+		//glTranslatef(0, 0, zoom);
+		glTranslatef(vec_objTransformInfo.at(i).x_trans, vec_objTransformInfo.at(i).y_trans, 0);
+		glRotatef(vec_objTransformInfo.at(i).x_angle, 1, 0, 0);
+		glRotatef(vec_objTransformInfo.at(i).y_angle, 0, 1, 0);
+		glTranslatef(-vec_objTransformInfo.at(i).center.x()
+			,-vec_objTransformInfo.at(i).center.y()
+			,-vec_objTransformInfo.at(i).center.z());
+
+
+
+		//Determine the specularity, emissivity, and shininess parameters, based on
+		//variables that can be toggled with keystrokes
+
+		float specularity = 1;
+		float emissivity = 0;
+		float shininess = 12;
+
+		GLfloat materialColor[4];
+		if(!f_multiple_obj)
+		{
+			materialColor[0] = 0.2f;
+			materialColor[1] = 0.2f;
+			materialColor[2] = 1.0f;
+			materialColor[3] = 1.0f;
+		}
+		else if(selected_obj_id != i)
+		{
+			//nothing is selected
+			int color_type = i%3;
+			if(color_type == 1)
+			{
+				//red
+				materialColor[0] = 1.0f;
+				materialColor[1] = 0.2f;
+				materialColor[2] = 0.2f;
+				materialColor[3] = 1.0f;
+			}
+			else if(color_type ==2)
+			{
+				//yellow
+				materialColor[0] = 1.0f;
+				materialColor[1] = 1.0f;
+				materialColor[2] = 0.2f;
+				materialColor[3] = 1.0f;
+			}
+			else if(color_type ==0)
+			{
+				//blue
+				materialColor[0] = 0.2f;
+				materialColor[1] = 0.2f;
+				materialColor[2] = 1.0f;
+				materialColor[3] = 1.0f;
+			}
+			
+		}
+		else if(selected_obj_id == i)
+		{
+			materialColor[0] = 1.0f;
+			materialColor[1] = 1.0f;
+			materialColor[2] = 1.0f;
+			materialColor[3] = 1.0f;
+
+			specularity = 0.0f;
+			emissivity = 1.0f;
 		}
 		
-		for(int j = 0; j < teapot.getVertexSize(i); j+= 1){
-			point = teapot.getVertex(i, j).p;
-			normal = teapot.getVertex(i, j).n;
-			//glNormal3f(-normal[0], -normal[1], -normal[2]);
-			glNormal3f(normal[0], normal[1], normal[2]);
-			glVertex3f(point[0], point[1], point[2]);
+		//The specular (shiny) component of the material
+		GLfloat materialSpecular[] = {specularity, specularity, specularity, 1.0f};
+		//The color emitted by the material
+		GLfloat materialEmission[] = {emissivity, emissivity, emissivity, 1.0f};
+
+		glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, materialColor);
+		glMaterialfv(GL_FRONT, GL_SPECULAR, materialSpecular);
+		glMaterialfv(GL_FRONT, GL_EMISSION, materialEmission);
+		glMaterialf(GL_FRONT, GL_SHININESS, shininess); //The shininess parameter
+
+		
+
+		Eigen::Vector3f point;
+		Eigen::Vector3f normal;
+
+		int poly_size = vec_polyVector.at(i).getPolySize();
+		for (int k = 0; k < poly_size; k += 1){
+			//cout << "Polygon " << i << ":" << endl;
+			if(_hiddenLine){
+				glBegin(GL_LINE_LOOP);
+			} else {
+				glBegin(GL_POLYGON);
+			}
+
+			for(int j = 0; j < vec_polyVector.at(i).getVertexSize(k); j+= 1){
+				point = vec_polyVector.at(i).getVertex(k, j).p;
+				normal = vec_polyVector.at(i).getVertex(k, j).n;
+				//glNormal3f(-normal[0], -normal[1], -normal[2]);
+				glNormal3f(normal[0], normal[1], normal[2]);
+				glVertex3f(point[0], point[1], point[2]);
+			}
+			glEnd();
 		}
-		glEnd();
+
+		glPopMatrix();
+
+
 	}
 
-	glPopMatrix();
 	glutSwapBuffers();
 }
 
@@ -295,59 +380,65 @@ void SpecialKeys(int key, int x, int y)
 	int state;
 	state=glutGetModifiers();
 	if (state != GLUT_ACTIVE_SHIFT){
-		if (key == GLUT_KEY_LEFT)
+		if(selected_obj_id < num_objs)
 		{
-			y_angle = y_angle + 1;
-			if (y_angle > 360) {
-				y_angle -= 360;
+			if (key == GLUT_KEY_LEFT)
+			{
+				vec_objTransformInfo.at(selected_obj_id).y_angle += 1;
+				if (vec_objTransformInfo.at(selected_obj_id).y_angle > 360) {
+					vec_objTransformInfo.at(selected_obj_id).y_angle -= 360;
+				}
+				glutPostRedisplay();
 			}
-			glutPostRedisplay();
-		}
-		if (key == GLUT_KEY_RIGHT)
-		{
-			y_angle = y_angle - 1;
-			if (y_angle < 0) {
-				y_angle += 360;
+			if (key == GLUT_KEY_RIGHT)
+			{
+				vec_objTransformInfo.at(selected_obj_id).y_angle -= 1;
+				if (vec_objTransformInfo.at(selected_obj_id).y_angle < 0) {
+					vec_objTransformInfo.at(selected_obj_id).y_angle += 360;
+				}
+				glutPostRedisplay();
 			}
-			glutPostRedisplay();
-		}
-		if (key == GLUT_KEY_UP)
-		{
-			x_angle = x_angle + 1;
-			if (x_angle > 360) {
-				x_angle -= 360;
+			if (key == GLUT_KEY_UP)
+			{
+				vec_objTransformInfo.at(selected_obj_id).x_angle += 1;
+				if (vec_objTransformInfo.at(selected_obj_id).x_angle > 360) {
+					vec_objTransformInfo.at(selected_obj_id).x_angle -= 360;
+				}
+				glutPostRedisplay();
 			}
-			glutPostRedisplay();
-		}
-		if (key == GLUT_KEY_DOWN)
-		{
-			x_angle = x_angle - 1;
-			if (x_angle < 0) {
-				x_angle += 360;
+			if (key == GLUT_KEY_DOWN)
+			{
+				vec_objTransformInfo.at(selected_obj_id).x_angle -= 1;
+				if (vec_objTransformInfo.at(selected_obj_id).x_angle < 0) {
+					vec_objTransformInfo.at(selected_obj_id).x_angle += 360;
+				}
+				glutPostRedisplay();
 			}
-			glutPostRedisplay();
 		}
 	} else {
-		const float trans_step = 0.1f;
-		if (key == GLUT_KEY_UP)
+		if(selected_obj_id < num_objs)
 		{
-			y_trans -= trans_step;
-			glutPostRedisplay();
-		}
-		if (key == GLUT_KEY_DOWN)
-		{
-			y_trans += trans_step;
-			glutPostRedisplay();
-		}
-		if (key == GLUT_KEY_LEFT)
-		{
-			x_trans -= trans_step;
-			glutPostRedisplay();
-		}
-		if (key == GLUT_KEY_RIGHT)
-		{
-			x_trans += trans_step;
-			glutPostRedisplay();
+			const float trans_step = 0.1f;
+			if (key == GLUT_KEY_UP)
+			{
+				vec_objTransformInfo.at(selected_obj_id).y_trans -= trans_step;
+				glutPostRedisplay();
+			}
+			if (key == GLUT_KEY_DOWN)
+			{
+				vec_objTransformInfo.at(selected_obj_id).y_trans += trans_step;
+				glutPostRedisplay();
+			}
+			if (key == GLUT_KEY_LEFT)
+			{
+				vec_objTransformInfo.at(selected_obj_id).x_trans -= trans_step;
+				glutPostRedisplay();
+			}
+			if (key == GLUT_KEY_RIGHT)
+			{
+				vec_objTransformInfo.at(selected_obj_id).x_trans += trans_step;
+				glutPostRedisplay();
+			}
 		}
 	}
 }
@@ -365,17 +456,20 @@ void Motion(int x,int y)
 	}
 	else
 	{
-		if( Buttons[0] )
+		if(selected_obj_id < num_objs)
 		{
-			x_angle += (float) 0.5f * diffy;
-			y_angle += (float) 0.5f * diffx;		
-		}
-		else
-			if( Buttons[1] )
+			if( Buttons[0] )
 			{
-				x_trans += (float) 0.05f * diffx;
-				y_trans -= (float) 0.05f * diffy;
+				vec_objTransformInfo.at(selected_obj_id).x_angle += (float) 0.5f * diffy;
+				vec_objTransformInfo.at(selected_obj_id).y_angle += (float) 0.5f * diffx;		
 			}
+			else
+				if( Buttons[1] )
+				{
+					vec_objTransformInfo.at(selected_obj_id).x_trans += (float) 0.05f * diffx;
+					vec_objTransformInfo.at(selected_obj_id).y_trans -= (float) 0.05f * diffy;
+				}
+		}
 	}
 	glutPostRedisplay();
 }
@@ -404,29 +498,120 @@ void Mouse(int b,int s,int x,int y)
 
 
 int main(int argc, char** argv) {
-	BezReader bezReader;
-	bezReader.ReadBezFile(argv[1]);
 
-	if(argc > 3)
+	if(strcmp(argv[1],"-m") == 0)
 	{
+		//	0	 1	2			3		4		5
+		//./ASS3 -m scene.scene 0.1(step) [-a  [0.01] ]
+
+		//multiple object
+		f_multiple_obj = true;
+		//read a scene file
+		bool f_adaptive = false;
+
+		float step = atof(argv[3]);
+
+		ifstream in;
+		in.open(argv[2]);
+
+
 		float error = 0.01;
-		if(argc>4)
+		if(argc > 4)
 		{
-			error = atof(argv[4]);
+			
+			if(argc>5)
+			{
+				error = atof(argv[5]);
+			}
+
+			if(strcmp(argv[4], "-a") == 0)
+			{
+				f_adaptive = true;
+				
+			} 
 		}
 
-		if(strcmp(argv[3], "-a") == 0)
+		string line;
+
+		int last_id = -1;
+		while(in>>line)
 		{
-			bezReader.BuildPolyVector_Adaptive(teapot,0.01,atof(argv[2]));
-		} 
+			//in>>line;
+			if(line == "")
+			{
+				break;
+			}
+
+			vec_polyVector.push_back(PolyVector());
+			vec_objTransformInfo.push_back(ObjTransformInfo());
+			last_id++;
+
+			BezReader bezReader;
+			bezReader.ReadBezFile(line.c_str());
+			if(f_adaptive)
+			{
+				bezReader.BuildPolyVector_Adaptive(vec_polyVector.at(last_id),error,step);
+			}
+			else
+			{
+				bezReader.BuildPolyVector_Uniform(vec_polyVector.at(last_id),step);
+			}
+			
+			vec_objTransformInfo.at(last_id).center = getCenter(vec_polyVector.at(last_id));
+
+		}
+
+
+		in.close();
+
+
+		vector<ObjTransformInfo>::iterator iter;
+		for(iter = vec_objTransformInfo.begin();iter!= vec_objTransformInfo.end();++iter)
+		{
+			center += iter->center ;
+		}
+		num_objs = vec_objTransformInfo.size();
+		center /= num_objs;
+
+		
+		selected_obj_id = num_objs;
 	}
-	else 
+	else
 	{
-		bezReader.BuildPolyVector_Uniform(teapot,atof(argv[2]));
+		//ugly implementation...
+		f_multiple_obj = false;
+
+		vec_polyVector.push_back(PolyVector());
+		vec_objTransformInfo.push_back(ObjTransformInfo());
+
+
+		BezReader bezReader;
+		bezReader.ReadBezFile(argv[1]);
+
+		if(argc > 3)
+		{
+			float error = 0.01;
+			if(argc>4)
+			{
+				error = atof(argv[4]);
+			}
+
+			if(strcmp(argv[3], "-a") == 0)
+			{
+				bezReader.BuildPolyVector_Adaptive(vec_polyVector.at(0),error,atof(argv[2]));
+			} 
+		}
+		else 
+		{
+			bezReader.BuildPolyVector_Uniform(vec_polyVector.at(0),atof(argv[2]));
+		}
+
+		num_objs = 1;
+		center = getCenter(vec_polyVector.at(0));
+		vec_objTransformInfo.at(0).center = center;
+
+		selected_obj_id = 0;
 	}
-
-
-	center = getCenter(teapot);
 	
 
 	glutInit(&argc, argv);
